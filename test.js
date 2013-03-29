cdd947d7cf334a1e713c472fd1c4347b42770c05
Dan Larsen <dan@techba.se>
2013-03-29 22:53:43 +0100

    Some fixes and tests

--- test.js
+++ test.js
@@ -7,6 +7,8 @@ http.createServer(function (req, res) {
   if(cookies.mycookie1 != undefined) {
     console.log('mycookie1 is set');
     console.log('mycookie1 value is:', cookies.mycookie1.value);
+    console.log(cookies.mycookie1.toObject());
+
   } else {
     console.log('mycookie1 is NOT set');
   }
@@ -14,13 +16,22 @@ http.createServer(function (req, res) {
   if(cookies['mycookie2'] != undefined) {
     console.log('mycookie2 is set');
     console.log('mycookie2 value is:', cookies['mycookie2']['value']);
-    
+    cookies['mycookie2'].expires = new Date();
+    cookies['mycookie2'].expires.setDate(cookies['mycookie2'].expires.getDate()+5);
+    console.log(cookies['mycookie2'].toObject());
+
   } else {
     console.log('mycookie2 is NOT set');
   }
+
+  cookies.mycookie2.secure = 1;
+  cookies.mycookie2.httponly = 1;
   
-  console.log(cookies.toString());
-  
+  console.log(cookies.toObject());
+  console.log(cookies.toObjects());
+  console.log(cookies['mycookie2'].toObject());
+
+  console.log(cookies['mycookie2'].toString());
   
   res.writeHead(200, {'Content-Type': 'text/plain', 'Set-Cookie': ['mycookie1=value1', 'mycookie2=value2'] });
   res.end('Hello World\n');
