7479a62ae507b346d6bcb6341f1ff95506cffd57
Dan Larsen <dan@techba.se>
2013-05-12 22:50:08 +0200

    Added some escaping and removed a console.log

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var Cookies = require('./lib/cookies.js');
+var Cookies = require('cookie-tools');
 
 http.createServer(function (req, res) {
   var cookies = new Cookies(req.headers.cookie);
-  
+
   if(cookies.mycookie1 != undefined) {
     console.log('mycookie1 is set');
     console.log('mycookie1 value is:', cookies.mycookie1.value);
@@ -12,7 +12,7 @@ http.createServer(function (req, res) {
   } else {
     console.log('mycookie1 is NOT set');
   }
-  
+
   if(cookies['mycookie2'] != undefined) {
     console.log('mycookie2 is set');
     console.log('mycookie2 value is:', cookies['mycookie2']['value']);
@@ -26,13 +26,13 @@ http.createServer(function (req, res) {
 
   cookies.mycookie2.secure = 1;
   cookies.mycookie2.httponly = 1;
-  
+
   console.log(cookies.toObject());
   console.log(cookies.toObjects());
   console.log(cookies['mycookie2'].toObject());
 
   console.log(cookies['mycookie2'].toString());
-  
+
   res.writeHead(200, {'Content-Type': 'text/plain', 'Set-Cookie': ['mycookie1=value1', 'mycookie2=value2'] });
   res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
